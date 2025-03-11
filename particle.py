dd067c2dc4e2e9e3a0df27e3ea7198075f3d9e75
Santi <santiramb@gmail.com>
2025-03-11 11:26:45 +0100

    Refactor np.array initialization and update_position

--- particle.py
+++ particle.py
@@ -1,20 +1,19 @@
+import numpy as np
+
 class Particle:
-    def __init__(self):
-        self.position = []          # Particle position, represents a possible solution
-        self.velocity = []          # Particle velocity, represents how the link configuration changes
-        self.p_best = []            # Best individual position
-        self.fitness_best = -1      # Best individual fitness value
-        self.fitness = -1           # Individual fitness
+    def __init__(self, num_links):
+        self.position = np.empty(num_links)   # Particle position, represents a possible solution
+        self.velocity = np.empty(num_links)   # Particle velocity, represents how the link configuration changes
+        self.p_best = np.empty(num_links)     # Best individual position
+        self.fitness_best = float('inf')      # Best individual fitness value
+        self.fitness = float('inf')           # Individual fitness
 
-    def evaluate(self, costFunc):
-        self.fitness = costFunc
+    def evaluate(self, cost_func):
+        self.fitness = cost_func
 
-        if self.fitness < self.fitness_best or self.fitness_best == -1:
+        if self.fitness < self.fitness_best:
             self.p_best = self.position
             self.fitness_best = self.fitness
 
-    def update_position(self, position):
-        num_links = 8
-
-        for i in range(0, num_links):
-            self.position[i] = position[i]
+    def update_position(self):
+        self.position = np.where(self.velocity > 0.5, 1-self.position, self.position)
